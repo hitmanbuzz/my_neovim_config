c912e7617c5137908d595883347fe7bb8b280d7c
hitmanbuzz <moirangthemhenthoiba30@gmail.com>
2024-12-02 04:28:34 +0530

    add necessary to commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# My Neovim Lua Config
+
+The config is made from the base LazyVim config.
+
+LazyVim Website: [LazyVim](https://www.lazyvim.org/)
+
+- *Moirangthem Henthoiba*
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,48 @@
+vim.o.background = "dark"
+
+--local colorscheme = require("lazyvim.plugins.colorscheme")
+return {
+  { "AlexvZyl/nordic.nvim" },
+
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+
+  { "ellisonleao/gruvbox.nvim" },
+
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = false
+    end,
+  },
+
+  {
+    "Mofiqul/adwaita.nvim",
+    lazy = false,
+    priority = 1000,
+
+    config = function()
+      vim.g.adwaita_darker = true
+    end,
+  },
+
+  {
+    "xiantang/darcula-dark.nvim",
+  },
+  {
+    "HoNamDuong/hybrid.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "hybrid",
+    },
+  },
+}
